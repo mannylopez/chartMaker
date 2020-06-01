14b0886d2f8be0d83c92df9c3dfe05c708a8aace
mannylopez <manuel.est.lopez@gmail.com>
2020-06-01 09:48:43 -0700

    Add business logic function to take in the color and step number and output the html. Add in the closing tag when the End button is pressed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,49 @@
 // Business logic
 const addColor = function(color, number) {
-  return color + number
+  
+  const colors = {
+    blue: '174,214,241',
+    red: '245,183,177',
+    yellow: '249,231,159',
+    green: '171,235,198'
+  };
+  let code = `<tr> \
+      <td class="wysiwyg-text-align-center" style="background-color: rgb(${colors[color]});width: 5.0%;text-align: center;vertical-align: top;"> \
+        <span class="wysiwyg-color-black wysiwyg-font-size-medium">${number}</span> \
+      </td> \
+      <td class="wysiwyg-text-align-center" style="width: 95.0%;background-color: rgb(247,247,247);text-align: left;vertical-align: top;"> \
+        <p></p> \
+      </td> \
+    </tr>`;
+
+  return code
+
 }
 
 // User interface logic
 $(document).ready(function() {
-  $("button").click(function(event) {
+
+  $(".button").click(function(event) {
     event.preventDefault();
     
     let number = $("#numberInput").val()
-    console.log(number);
+    
 
     let color = $(this).attr("id");
-    console.log(color);
+
+    let presentValue = $("textarea").val()
 
     let result = addColor(color, number);
 
-    $("#output").text(result);
+    $("#output").text(presentValue + result);
+  });
 
+  $("#end").click(function(event) {
+    event.preventDefault();
+
+    let presentValue = $("textarea").val()
+
+    let closingTag = `</tbody></table>`
+    $("#output").text(presentValue + closingTag);
   });
 });
-
-// $(document).ready(function() {
-//   $("#number-to-sentence").submit(function(event) {
-//     event.preventDefault();
-
-//     let userInput = parseInt($("#user-input").val());
-//     let result = rogersConverter(userInput);
-    
-//     $(".rogers-output").text();
-//     $(".rogers-output").text(result);
-//   });
-// });
-
-
-
-// // One function to rule them all
-// function addColor(colorName){
-//   var step = document.getElementById("numberInput").value;
-//   console.log(step);
-//   var colors = {
-//    BLUE: '174,214,241',
-//    RED: '245,183,177',
-//    YELLOW: '249,231,159',
-//    GREEN: '171,235,198'
-//   };
-
-//   var code = `<tr> \
-//       <td class="wysiwyg-text-align-center" style="background-color: rgb(${colors[colorName]});width: 5.0%;text-align: center;vertical-align: top;"> \
-//         <span class="wysiwyg-color-black wysiwyg-font-size-medium">${step}</span> \
-//       </td> \
-//       <td class="wysiwyg-text-align-center" style="width: 95.0%;background-color: rgb(247,247,247);text-align: left;vertical-align: top;"> \
-//         <p></p> \
-//       </td> \
-//     </tr>`;
-//   //We first get the current value of the textarea
-//   var x = document.getElementById("htmlOutput").value;
-
-//   //Then we concatenate the string "content" onto it
-//   document.getElementById("htmlOutput").value = x + code;
-//   // document.getElementById("htmlOutput").innerHTML = x + blueTable;
-// }
-
-// function endTag(){
-//   //We first get the current value of the textarea
-//   var x = document.getElementById("htmlOutput").value;
-//   var end = `</tbody></table>`
-//   //Then we concatenate the string "content" onto it
-//   console.log(end)
-//   document.getElementById("htmlOutput").value = x + end;
-// }
